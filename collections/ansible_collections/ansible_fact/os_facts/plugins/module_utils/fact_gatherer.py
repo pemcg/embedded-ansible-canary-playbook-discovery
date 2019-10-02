5366f4f87288e99c497831a71731f800c40396fe
Andrew J Huffman <ahuffman@redhat.com>
2019-10-02 16:26:20 -0400

    update warnings

--- collections/ansible_collections/ansible_fact/os_facts/plugins/module_utils/fact_gatherer.py
+++ collections/ansible_collections/ansible_fact/os_facts/plugins/module_utils/fact_gatherer.py
@@ -16,10 +16,10 @@ class FactGatherer(AnsibleModule):
             if cmd is not None:
                 return cmd
             else:
-                # self.warn(msg="Unable to find {} command. Is it installed?".format(command))
+                self.warn("Unable to find {} command. Is it installed?".format(command))
                 self.exit_json(msg="Unable to find {} command. Is it installed?".format(command), skipped=True)
         except Exception as e:
-            # self.warn(msg="Unable to find {} command: {}, is it installed?".format(command, e))
+            self.warn("Unable to find {} command: {}, is it installed?".format(command, e))
             self.exit_json(msg="Unable to find {} command: {}, is it installed?".format(command, e), skipped=True)
 
     ####### End Of Helper Methods ########
