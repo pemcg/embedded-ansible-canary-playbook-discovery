2588376a6b5a2b3430acb6af9c5a44deb581addf
Andrew J Huffman <ahuffman@redhat.com>
2019-10-08 11:31:48 -0400

    add to script to ensure the skeleton is always up to date

--- mkappsigrole.sh
+++ mkappsigrole.sh
@@ -3,8 +3,12 @@
 path="$(pwd)"
 init_path="roles"
 
+# ensure our skeleton is fully up to date and on correct branch :)
+cd ../ansible-canary-role-development-application-signature-skeleton
+git pull origin master
+git checkout master
+cd $path
+
 if ansible-galaxy init --role-skeleton=../ansible-canary-role-development-application-signature-skeleton/ --init-path=$init_path amf-application-signature-$1; then
 echo "role amf-application-signature-$1" "created at" $path"/"$init_path/amf-application-signature-$1
-else
- :
 fi
